ddf514b1c84b30b8cf08383ef30b995a493098a9
Pikachu <jouxiong15@gmail.com>
2021-05-05 22:18:55 -0500

    finished model data

--- models/Comment.js
+++ models/Comment.js
@@ -1,14 +1,36 @@
-const { Sequelize, Model, DataTypes } = require("sequelize");
+const { Sequelize, Model, DataTypes, INTEGER } = require("sequelize");
 const sequelize = require("../config/connection");
 class Comment extends Model{}
+
 Comment.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
     body: {
         type: DataTypes.STRING,
         allowNull: false
     },
+    post_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'post',
+            key: 'id',
+        }
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id',
+        }
+    }
 },
     {
-        sequelize
+        sequelize,
+        timestamps: true
     }
 )
 
--- models/Post.js
+++ models/Post.js
@@ -1,12 +1,31 @@
 const { Sequelize, Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
+
 class Post extends Model{}
+
 Post.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
     title: DataTypes.STRING,
-    body: DataTypes.STRING
+    body: DataTypes.STRING,
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id',
+        }
     },
+},
     {
-        sequelize
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post',
     }
 )
 
