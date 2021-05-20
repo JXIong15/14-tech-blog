b2339a43f9d2a4115a0807969768d8a0169102c8
Pikachu <jouxiong15@gmail.com>
2021-05-19 23:16:20 -0500

    delete post works

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -35,69 +35,28 @@ router.get('/create', withAuth, (req, res) => {
         include: [User],
       },
     ],
-  //   where: {
-  //       user_id: req.session.user_id,
-  //   }
   })
   .then((postData) => {
-      // Serialize data so the template can read it
       const posts = postData.map((post) => post.get({ plain: true }));
 
-      // Pass serialized data and session flag into template
       res.render('new-post', {posts, loggedIn: true});
   })
 .catch ((err) => {res.status(500).json(err)})
 });
 
-// DOES NOT WORK
-// edits existing post
-router.put('/:id', withAuth, (req, res) => {
-  Post.findByPk({
-    where: {
-      id: req.params.id,
-        // user_id: req.session.user_id,
-    },
-    include: [User, {
-      model: Comment,
-      include: [User],
-    }]
-  })
+
+
+// edits post
+router.get('/edit-post/:id', withAuth, (req, res) => {
+  Post.findByPk(req.params.id,{})
   .then((postData) => {
     if (!postData) {
       res.status(404).json({ message: 'No post found with that id' });
       return;
     }
-      // Serialize data so the template can read it
-      const posts = postData.get({ plain: true });
+      const post = postData.get({ plain: true });
 
-      // Pass serialized data and session flag into template
-      res.render('edit-post', {posts, loggedIn: true});
-  })
-.catch ((err) => {res.status(500).json(err)})
-});
-
-// deletes post - DOES NOT WORK
-router.put('/delete/:id', withAuth, (req, res) => {
-  Post.findByPk({
-    where: {
-      id: req.params.id,
-        // user_id: req.session.user_id,
-    },
-    include: [User, {
-      model: Comment,
-      include: [User],
-    }]
-  })
-  .then((postData) => {
-    if (!postData) {
-      res.status(404).json({ message: 'No post found with that id' });
-      return;
-    }
-      // Serialize data so the template can read it
-      const posts = postData.get({ plain: true });
-
-      // Pass serialized data and session flag into template
-      res.render('edit-post', {posts, loggedIn: true});
+      res.render('edit-post', {post, loggedIn: true});
   })
 .catch ((err) => {res.status(500).json(err)})
 });
