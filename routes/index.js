b29f8336e4ad29b7ad7aab7f7bab4e6a32640a7c
Pikachu <jouxiong15@gmail.com>
2021-05-22 14:39:16 -0500

    edited homeroutes js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
+const homeRoutes = require('./homeroutes');
 const dashboardRoutes = require('./dashboardRoutes');
 
 router.use('/', homeRoutes);
